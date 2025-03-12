1215f71dc45d775855b2e534b962ca02721fdbb4
IvetteGonzalez <goji85@gmail.com>
2025-03-11 23:10:00 -0600

    Update reportes.py
    Graficas

--- reportes.py
+++ reportes.py
@@ -7,7 +7,8 @@ from flet.plotly_chart import PlotlyChart
 class UIReportes(ft.Container):
     def __init__(self, page:ft.Page):
         super().__init__(expand=True)
-        self.page = page
+        self.page=page
+        self.page.padding = 0
 
         # Variables
         self.pacientes_data = pd.DataFrame(pd.read_csv("data/pacientes.csv"))
@@ -18,7 +19,7 @@ class UIReportes(ft.Container):
         self.color_purple = ft.Colors.TEAL_ACCENT_700
         self.color_green = ft.Colors.TEAL_ACCENT_700
         self.bg_color = "#396564"
-        self.container_color = ft.Colors.WHITE
+        self.container_color = "#4ea3a1"
         self.color_navigation_bt = ft.Colors.LIGHT_GREEN_900
         self.color1_card = "#f46fd8"
         self.color2_card = "#64e4ed"
@@ -72,16 +73,22 @@ class UIReportes(ft.Container):
         )
 
         self.select_reporte = ft.DropdownM2(
-            label="Color",
-            hint_text="Choose your favourite color?",
-            on_change=self.dropdown_changed(),
+            label="Reportes",
+            hint_text="Selecciona el reporte deseado",
+            width= 600,
+            border_color= "white",
+            border_radius= 50,
+            padding=10,
+            alignment=ft.alignment.center,
+            on_change=self.dropdown_changed,
             options=[
-                ft.dropdownm2.Option("Red"),
-                ft.dropdownm2.Option("Green"),
-                ft.dropdownm2.Option("Blue"),
+                ft.dropdownm2.Option(1,"Reporte por Género"),
+                ft.dropdownm2.Option(2,"Reporte Movimiento de peso por paciente"),
+                ft.dropdownm2.Option(3,"Reporte por edades"),
             ],
             autofocus=True,
         )
+
 
         self.grafico_genero = ft.Container(
             content=ft.PieChart(
@@ -118,17 +125,21 @@ class UIReportes(ft.Container):
                 center_space_radius=0,
                 #on_chart_event=on_chart_event,
                 expand=True,
-            )
+            ),
+            offset=ft.transform.Offset(1, 0)
         )
         self.df_linea = px.data.gapminder().query("continent=='Oceania'")
         self.fig = px.line(self.df_linea, x="year", y="lifeExp", color="country")
-        self.grafico_peso= PlotlyChart(self.fig, expand=True)
+        self.grafico_peso= ft.Container(content=PlotlyChart(self.fig, expand=True),
+                                        offset=ft.transform.Offset(1,0)
+                                        )
 
         self.data_canada = px.data.gapminder().query("country == 'Canada'")
         self.fig_barras = px.bar(self.data_canada, x='year', y='pop')
-        self.grafico_edades=PlotlyChart(self.fig_barras,expand=True)
-
-
+        self.grafico_edades=ft.Container(
+            content=(PlotlyChart(self.fig_barras,expand=True)),
+            offset=ft.transform.Offset(1, 0)
+        )
 
 
 
@@ -136,14 +147,18 @@ class UIReportes(ft.Container):
         self.graficosframe = ft.Container(
             content=ft.Column(
                 horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+                alignment=ft.alignment.center,
                 controls=[
-                    ft.Text("Seleccione el reporte deseado:"),
+                    ft.Text("REPORTES",theme_style=ft.TextThemeStyle.DISPLAY_MEDIUM),
+                    ft.Text("Seleccione el reporte deseado:",theme_style=ft.TextThemeStyle.HEADLINE_MEDIUM),
+                    ft.Divider(height=2, color="green"),
                     self.select_reporte,
                     self.grafico_genero,
                     self.grafico_peso,
                     self.grafico_edades
 
-                ]
+                ],
+                offset=ft.transform.Offset(0,0)
             )
         )
 
@@ -168,6 +183,7 @@ class UIReportes(ft.Container):
                 ]
             )
         )
+
 
         # Add components a panel principal
         self.page.add(
@@ -198,8 +214,8 @@ class UIReportes(ft.Container):
             bgcolor=ft.Colors.WHITE,
         )
 
-    def dropdown_changed(e):
-        print("on change en construcción")
+
+
 
     def badgefunction(icon, size):
         return ft.Container(
@@ -210,6 +226,38 @@ class UIReportes(ft.Container):
             border_radius=size / 2,
             bgcolor=ft.Colors.WHITE,
         )
+
+    def ocultar_chart(self):
+        self.grafico_genero.offset = ft.transform.Offset(1, 0),
+        self.grafico_peso.offset = ft.transform.Offset(1, 0),
+        self.grafico_edades.offset = ft.transform.Offset(1, 0)
+
+
+
+    def dropdown_changed(self,e):
+        print("on change en construcción")
+        print(self.select_reporte.value)
+
+        if self.select_reporte.value == 1:
+            print(1)
+            self.grafico_genero.offset = ft.transform.Offset(0, 0),
+            self.grafico_peso.offset = ft.transform.Offset(1, 0),
+            self.grafico_edades.offset = ft.transform.Offset(1, 0)
+
+        elif self.select_reporte.value == 2:
+            print(2)
+            self.grafico_genero.offset = ft.transform.Offset(1, 0),
+            self.grafico_peso.offset = ft.transform.Offset(0, 0),
+            self.grafico_edades.offset = ft.transform.Offset(1, 0)
+        elif self.select_reporte.value == 3:
+            print(3)
+            self.grafico_genero.offset = ft.transform.Offset(1, 0),
+            self.grafico_peso.offset = ft.transform.Offset(1, 0),
+            self.grafico_edades.offset = ft.transform.Offset(0, 0)
+
+
+        self.page.update()
+
 
 
 ft.app(target=lambda page:UIReportes(page))
